c24baf59191d84e419ff4507eae937d682210664
Rishabh Kumar <rishabh.kr432@gmail.com>
2019-12-03 14:55:06 +0000

    Update postscore.php

--- postscore.php
+++ postscore.php
@@ -7,7 +7,7 @@ $aiScore = pg_escape_string($_POST['aiScore']);
 
 
 // Connecting, selecting database
-$dbconn = pg_connect("host=db.dcs.aber.ac.uk port=5432 dbname=cs25320_19_20_rik7 user=rik7 password=Lcmd66JY")
+$dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=Rishabh11@")
     or die('Could not connect: ' . pg_last_error());
 
 $result = pg_query($dbconn, "INSERT INTO scores(name, plScore, aiScore) VALUES('{$name}','{$plScore}','{$aiScore});");
