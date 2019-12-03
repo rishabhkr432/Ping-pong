f1f679b7f6e9354742148e9c6fbefb598c2875ce
Rishabh Kumar <rishabh.kr432@gmail.com>
2019-12-03 14:38:03 +0000

    minor changes

--- postscore.php
+++ postscore.php
@@ -1,20 +1,24 @@
 <?php 
+
+$name = pg_escape_string($_POST['name']);
+$plScore =  pg_escape_string($_POST['plScore']);
+$aiScore = pg_escape_string($_POST['aiScore']);
+
+
+
+// Connecting, selecting database
 $dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=Rishabh11@")
     or die('Could not connect: ' . pg_last_error());
-	
-if(!empty($_POST["name"] || $_POST["plScore"] || $_POST["aiScore"])){
-	
-}
-else{
-	echo "No data";
-	die();
-}
-$sql = "INSERT INTO scores (name, plScore, aiScore)
-		values('$name','$plScore','$aiScore')";
-		if($dbconn->query($sql)){
-			echo "new record added";
-		}
-		else{
-			echo "Error";
-		}
+
+$result = pg_query($dbconn, "INSERT INTO scores(name, plScore, aiScore) VALUES('{$name}','{$plScore}','{$aiScore});");
+	   
+	   if (!$result) { 
+            $errormessage = pg_last_error(); 
+            echo "Error with query: " . $errormessage; 
+            exit(); 
+        } 
+        pg_close(); 
+
+// Closing connection
+pg_close($dbconn);
 ?>
