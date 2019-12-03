7b2874f20535b820d99ca0d0d92719889068dd12
Rishabh Kumar <rishabh.kr432@gmail.com>
2019-12-03 14:18:20 +0000

    testing

--- postscore.php
+++ postscore.php
@@ -2,6 +2,20 @@
 $dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=Rishabh11@")
     or die('Could not connect: ' . pg_last_error());
 	
+if(!empty($_POST["name"] || $_POST["plScore"] || $_POST["aiScore"])){
+	
+}
+else{
+	echo "No data";
+	die();
+$sql = "INSERT INTO scores (name, plScore, aiScore)
+		values('$name','$plScore','$aiScore')";
+		if($dbconn->query($sql)){
+			echo "new record added";
+		}
+		else{
+			echo "Error"
+	
 
 
 ?>
